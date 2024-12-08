83961108589ce5c4cc26fced43d7c8f4b9866603
SamP9999 <154279024+SamP9999@users.noreply.github.com>
2024-12-08 15:58:37 -0400

    new commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+async function generateQuote(){
+  const response = await fetch('https://api.quotable.io/random');
+  const data = await response.json();
+  const quote = data.cotent;
+  document.querySelector('.quote').textContent = "${quote}";
+}
