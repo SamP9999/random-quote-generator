b2dda6ba9933530f57b689fd5ee38039aa67018e
SamP9999 <154279024+SamP9999@users.noreply.github.com>
2024-12-30 15:44:14 -0400

    added /quote to url

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 async function generateQuote() {
   try {
-    const response = await fetch('https://random-quote-generator-97lw.onrender.com');
+    const response = await fetch('https://random-quote-generator-97lw.onrender.com/quote');
     const data = await response.json();
     const quote = data.quote; // Access the quote property
     document.querySelector('.quote').textContent = quote; // Display the quote in the HTML
