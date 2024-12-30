2ac67d8f2cb1909910fe2caf7299aeaf90dbd3f0
SamP9999 <154279024+SamP9999@users.noreply.github.com>
2024-12-30 15:35:51 -0400

    added url

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 async function generateQuote() {
   try {
-    const response = await fetch('http://localhost:3000/quote');
+    const response = await fetch('https://random-quote-generator-97lw.onrender.com');
     const data = await response.json();
     const quote = data.quote; // Access the quote property
     document.querySelector('.quote').textContent = quote; // Display the quote in the HTML
--- server.mjs
+++ server.mjs
@@ -9,7 +9,7 @@ app.use(cors()); // Use the CORS middleware
 
 app.get('/quote', async (req, res) => {
   try {
-    const response = await fetch('https://random-quote-generator-97lw.onrender.com');
+    const response = await fetch('https://zenquotes.io/api/random');
     const data = await response.json();
     console.log("Quote data:", data); // Log the data to inspect it
     res.json({ quote: data[0].q }); // Send the quote as a JSON response
